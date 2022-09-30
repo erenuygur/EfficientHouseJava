25a9e57da641d890564f83fba6e415b5493c1079
Eren <ernuygur@gmail.com>
2022-09-30 17:26:52 +0300

    Chapter1 Q5 solution

---
+++ src/homeworks/chapter1/Q5.java
@@ -0,0 +1,10 @@
+package homeworks.chapter1;
+
+public class Q5 {
+    public static void main(String[] args)
+    {
+        String text = "I hate you";
+
+        System.out.println(text.replace("hate", "love"));
+    }
+}
