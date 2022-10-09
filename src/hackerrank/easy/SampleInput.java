27bfe3d5a3c51fbb6e5d3821a6e3005b9a8fa26b
Eren <ernuygur@gmail.com>
2022-10-09 23:12:58 +0300

    Scanner class and its usage

---
+++ src/hackerrank/easy/SampleInput.java
@@ -0,0 +1,17 @@
+package hackerrank.easy;
+
+public class SampleInput {
+    public static void main(String[] args) {
+        java.util.Scanner scan = new java.util.Scanner(System.in);
+
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
