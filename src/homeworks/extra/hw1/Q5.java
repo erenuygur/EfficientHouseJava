f28cfcb422c80fb805ecf1fcf4acb5c204e479c5
Eren <ernuygur@gmail.com>
2022-10-30 19:57:48 +0300

    isPrime

---
+++ src/homeworks/extra/hw1/Q5.java
@@ -0,0 +1,41 @@
+package homeworks.extra.hw1;
+
+public class Q5 {
+    public static void main(String[] args)
+    {
+        run();
+    }
+
+    public static void run()
+    {
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+
+        while (true) {
+            System.out.print("Sayi giriniz:");
+            long l = kb.nextLong();
+
+            if (l == 0)
+                return;
+
+            if (isPrime(l))
+                System.out.printf("%d sayisi asaldir%n", l);
+            else
+                System.out.printf("%d sayisi asal degildir%n", l);
+        }
+    }
+
+    public static boolean isPrime(long a)
+    {
+        if (a <= 1)
+            return false;
+
+        if (a % 2 == 0)
+            return a == 2;
+
+        for (long i = 3; i * i <= a; i += 2)
+            if (a % i == 0)
+                return false;
+
+        return true;
+    }
+}
