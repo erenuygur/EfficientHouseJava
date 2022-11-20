c70cf70ac41c03a96b3946454c84502b24f2da3e
Eren <ernuygur@gmail.com>
2022-11-20 23:07:02 +0300

    switch-case intro

---
+++ src/lessons/l10/SwitchIntro.java
@@ -0,0 +1,32 @@
+package lessons.l10;
+
+public class SwitchIntro {
+    public static void main(String[] args) {
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+
+        System.out.print("Postal code: ");
+        int postalCode = kb.nextInt();
+
+        switch (postalCode) {
+            case 55060:
+                System.out.println("Samsun");
+                break;
+            case 35580:
+                System.out.println("Izmir");
+                break;
+            case 25100:
+                System.out.println("Erzurum");
+                break;
+            case 34212:
+                System.out.println("Istanbul");
+                break;
+            case 42000:
+                System.out.println("Konya");
+                break;
+            default:
+                System.out.println("Invalid Postal code");
+        }
+    }
+}
+
+
