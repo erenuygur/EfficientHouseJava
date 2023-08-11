129af01a0d0864937a54579c8f4dc07db552b20e
SametKula <sametkula@proton.me>
2023-08-11 21:30:10 +0300

    Overview (arrays): NumericLotteryShop reviewed and organized. - New methods added for division of tasks. - The new methods are isOfAge, takeCustomerAge, takeCustomerWantedRowCount, tryPayment. - getInfoFromCustomer modified for division of tasks.

--- src/lessons/arrays/lottery/NumericLotteryShop.java
+++ src/lessons/arrays/lottery/NumericLotteryShop.java
@@ -6,52 +6,79 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class NumericLotteryShop {
-    public static Random random = new Random();
     public static Scanner kb = new Scanner(System.in);
+    public static Random random = new Random();
     public static int costOfRow = 10;
 
     public static int[] generateNumericLottery()
     {
         return new NumericLottery().getNums(random);
     }
-
     public static void getInfoFromCustomer(Customer customer)
     {
-        System.out.println("Clerk: How old are you ?");
-        System.out.printf("Customer: %d%n", customer.age);
+        int customerAge = takeCustomerAge(customer);
+        System.out.printf("Customer: %d%n", customerAge);
 
-        if (customer.age < 19) {
+        if (!isOfAge(customerAge)) {
             System.out.println("You are not allowed to play");
             return;
         }
 
-        System.out.print("Clerk: How many row do you want ?");
-        int rowCount = Integer.parseInt(kb.nextLine());
+        int rowCount = takeCustomerWantedRowCount(customer);
         System.out.printf("Customer: %d%n", rowCount);
 
         int price = rowCount * costOfRow;
 
-        System.out.printf("Clerk: %d $ please%n", price);
+        boolean isPaymentSucceeded = tryPayment(customer, price);
 
-        if (!customer.isSufficientBalance(price))  {
+        if(!isPaymentSucceeded){
+            System.out.println("I don't have enough money.Have a nice day.");
             return;
         }
 
-        customer.pay(price);
-
-        System.out.println("----------------Lottery Machine Sound----------------");
-        System.out.println("-----------------------------------------------------");
-
-        for (int i = 0; i < rowCount; ++i) {
-            ArrayMethods.displayArray(generateNumericLottery());
-        }
-
-        System.out.println("-----------------------------------------------------");
-
+        runLotteryMachine(rowCount);
     }
-
+    public static boolean isOfAge(int customerAge)
+    {
+        return customerAge > 18;
+    }
     public static void run()
     {
         getInfoFromCustomer(new Customer(1000, 22));
+    }
+
+    public static void runLotteryMachine(int rowCount)
+    {
+        System.out.println("----------------Lottery Machine Sound----------------\n" +
+                "-----------------------------------------------------");
+
+        for (int i = 0; i < rowCount; ++i)
+            ArrayMethods.displayArray(generateNumericLottery());
+
+        System.out.println("-----------------------------------------------------");
+    }
+
+    public static int takeCustomerAge(Customer customer)
+    {
+        System.out.println("Clerk: How old are you ?");
+        return customer.age;
+    }
+
+    public static int takeCustomerWantedRowCount(Customer customer)
+    {
+        System.out.print("Clerk: How many row do you want ?");
+        return Integer.parseInt(kb.nextLine());
+    }
+
+    public static boolean tryPayment(Customer customer, int price)
+    {
+        boolean flag = true;
+        System.out.printf("Clerk: %d $ please%n", price);
+
+        if (!customer.isSufficientBalance(price))
+            flag = false;
+
+        customer.pay(price);
+        return flag;
     }
 }
